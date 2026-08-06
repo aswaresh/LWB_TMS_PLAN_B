e51092b83e68e061f6530b745752fd2bc8793211
aswaresh <aswaresh.r@gmail.com>
2026-08-06 18:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -418,4 +418,8 @@ def handle_board_update(data):
 # ----------------------------------
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5000
+    )
