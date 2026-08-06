0656951bd21a5633cf38a56992b20a937cedffa5
aswaresh <aswaresh.r@gmail.com>
2026-08-06 19:01:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ from flask_socketio import (
 )
 load_dotenv()
 app = Flask(__name__)
-socketio = SocketIO(app)
-
+socketio = SocketIO(
+    app,
+    cors_allowed_origins="*"
+)
 
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
