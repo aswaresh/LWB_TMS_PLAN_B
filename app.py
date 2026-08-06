02bf5bbcd3089f03a9535ab503b953708cab4c23
aswaresh <aswaresh.r@gmail.com>
2026-08-06 18:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,12 @@ socketio = SocketIO(app)
 
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "pool_pre_ping": True,
+    "pool_recycle": 300,
+}
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
